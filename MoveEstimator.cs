c60c5fe115d7c9ba9bd1d276c833f4f8ec5a2005
Оля Дружкова <Druzhkova@MacBook-Pro-Ola.local>
2024-09-14 16:08:01 -0400

    first commit

---
+++ MoveEstimator.cs
@@ -0,0 +1,20 @@
+﻿namespace MoveEstimator;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double baserate = 200;
+        double perhour = 150;
+        double permile = 2;
+        string hours = Console.ReadLine();
+        string miles = Console.ReadLine();
+        string[] x = hours.Split(" ");
+        string[] y = miles.Split(" ");
+        double xresult = Convert.ToDouble(x[0]);
+        double yresult = Convert.ToDouble(y[0]);
+        double px = baserate + perhour * xresult + permile * yresult;
+        string result = px.ToString("F2");
+        Console.WriteLine("$" + result);
+    }
+}
