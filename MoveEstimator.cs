99825254df3100a5bf001efc70f904757cd63f2c
Оля Дружкова <Druzhkova@MacBook-Pro-Ola.local>
2024-11-08 10:51:59 -0500

    update constantd and add output

--- MoveEstimator.cs
+++ MoveEstimator.cs
@@ -4,16 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        double baserate = 200;
-        double perhour = 150;
-        double permile = 2;
-        string hours = Console.ReadLine();
-        string miles = Console.ReadLine();
-        string[] x = hours.Split(" ");
-        string[] y = miles.Split(" ");
-        double xresult = Convert.ToDouble(x[0]);
-        double yresult = Convert.ToDouble(y[0]);
-        double px = baserate + perhour * xresult + permile * yresult;
+        const double BASE_RATE = 200;
+        const double PER_HOUR = 150;
+        const double PER_MILE = 2;
+        Console.WriteLine("Please Enter the hours: ");
+        double hours = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Please Enter the miles: ");
+        double miles = Convert.ToDouble(Console.ReadLine());
+        double px = BASE_RATE + PER_HOUR * hours + PER_MILE * miles;
         string result = px.ToString("F2");
         Console.WriteLine("$" + result);
     }
--- obj/Debug/net8.0/MoveEstimator.AssemblyInfo.cs
+++ obj/Debug/net8.0/MoveEstimator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MoveEstimator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c60c5fe115d7c9ba9bd1d276c833f4f8ec5a2005")]
 [assembly: System.Reflection.AssemblyProductAttribute("MoveEstimator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MoveEstimator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
